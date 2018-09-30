cb2c4c862f5c768df029315cb9e297a296d33856
pulkit6559 <pulkitarora7.fas@gmail.com>
2018-10-01 00:33:07 +0530

    .

--- models/item.py
+++ models/item.py
@@ -17,17 +17,8 @@ class ItemModel(db.Model):
 
     @classmethod
     def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor  = connection.cursor()
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query,(name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return {'item': {'name': row[0],'price': row[1]}}
-        
-
+        return ItemModel.query.filter_by(name=name)
+     
     @classmethod
     def insert(cls, item):
         connection = sqlite3.connect('data.db')
